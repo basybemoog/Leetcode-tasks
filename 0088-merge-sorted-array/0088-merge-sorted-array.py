4acaff8bf746e0127ff95a44c0c48683d55b9ff9
basybemoog <155531283+basybemoog@users.noreply.github.com>
2024-05-12 15:33:55 +0500

    Time: 14 ms (89.33%), Space: 11.7 MB (45.89%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        if n == 0 : return
+        length1 = len(nums1)
+        end_index = length1-1 
+        while n > 0 and m > 0:
+            if nums1[m-1] <= nums2[n-1]:
+                nums1[end_index] = nums2[n-1]
+                n -= 1
+            else:
+                nums1[end_index] = nums1[m-1] 
+                m-=1
+            end_index-=1    
+        while n > 0:
+            nums1[end_index] = nums2[n-1]
+            n-=1
+            end_index-=1    
