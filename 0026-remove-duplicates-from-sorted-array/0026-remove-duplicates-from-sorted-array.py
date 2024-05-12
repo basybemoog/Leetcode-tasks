30845cc032bb3aa00e660f0417c502a458ac7871
basybemoog <155531283+basybemoog@users.noreply.github.com>
2024-05-12 15:47:18 +0500

    Time: 58 ms (60.5%), Space: 13 MB (62.66%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i = 0
+        j = 1
+        while j <len(nums):
+            if nums[i] < nums[j]:
+                i+=1
+                nums[i] = nums[j]
+            j+=1
+        return i + 1
+        
