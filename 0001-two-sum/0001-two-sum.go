9f6bd8b44daa0f00dd8db68aa7f742f0a68dd2af
basybemoog <155531283+basybemoog@users.noreply.github.com>
2024-05-13 11:17:49 +0500

    Time: 50 ms (8.14%), Space: 3.6 MB (72.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,13 +1,13 @@
 func twoSum(nums []int, target int) []int {
-  var list []int
-    for i := 0; i < len(nums); i++ {
+	var list []int
+	for i := 0; i < len(nums); i++ {
 		for j := 0; j < len(nums)-1; j++ {
-			if nums[i] + nums[j] == target && j != i {
+			if nums[i]+nums[j] == target && j != i {
 				list = append(list, i)
 				list = append(list, j)
-         return list
+				return list
 			}
 		}
 	}
- return list
+	return list
 }
