f6b7a68150e80b46880a4751048bef95baede87f
basybemoog <155531283+basybemoog@users.noreply.github.com>
2024-05-15 18:27:42 +0500

    Time: 1 ms (79.81%), Space: 2.6 MB (91.39%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.go
@@ -0,0 +1,14 @@
+func generate(numRows int) [][]int {
+    	var rows = make([][]int, numRows)
+	for i:=0;i<numRows;i++{
+		rows[i]=make([]int,i+1)
+		rows[i][0]=1
+		rows[i][i]=1
+	}
+	for i:=2;i<numRows;i++{
+		for j:=1;j<i;j++ {
+			rows[i][j]=rows[i-1][j-1]+rows[i-1][j]
+		}
+	}
+    return rows
+}
