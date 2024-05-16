f56de539666b346d38595e466a5197f2ab8b2d9d
basybemoog <155531283+basybemoog@users.noreply.github.com>
2024-05-16 15:33:56 +0500

    Time: 33 ms (29.85%), Space: 7.1 MB (8.28%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.go
+++ 0344-reverse-string/0344-reverse-string.go
@@ -1,8 +1,5 @@
 func reverseString(s []byte)  {
-	var swap byte
 	for start, end := 0, len(s)-1; start < end; start, end = start+1, end-1 {
-		swap = s[start]
-		s[start] = s[end]
-		s[end] = swap
+		s[start], s[end] = s[end], s[start]
 	}
 }
