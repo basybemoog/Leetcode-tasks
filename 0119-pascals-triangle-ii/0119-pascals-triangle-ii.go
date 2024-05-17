19341479524ef41c8233e5c38413e0e5490ad187
basybemoog <155531283+basybemoog@users.noreply.github.com>
2024-05-17 15:35:11 +0500

    Time: 0 ms (100.00%), Space: 2.4 MB (37.22%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.go
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.go
@@ -1,11 +1,11 @@
 func getRow(rowIndex int) []int {
-        	var rows = make([][]int, 34)
-	for i:=0;i<34;i++{
+        	var rows = make([][]int, rowIndex+1)
+	for i:=0;i<rowIndex+1;i++{
 		rows[i]=make([]int,i+1)
 		rows[i][0]=1
 		rows[i][i]=1
 	}
-	for i:=2;i<34;i++{
+	for i:=2;i<rowIndex+1;i++{
 		for j:=1;j<i;j++ {
 			rows[i][j]=rows[i-1][j-1]+rows[i-1][j]
 		}
